cddad4eea89bd16a4b42b94fc2ec41530af3a9dd
Evgen12412 <evgeny.krd2016@yandex.ru>
2023-01-12 21:41:37 +0300

    1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,72 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    String comand;
+    Statement statement = Util.getConnection().createStatement();
+
+    public UserDaoJDBCImpl() throws SQLException, ClassNotFoundException {
+    }
+
+    public void createUsersTable() throws SQLException {
+         comand = "CREATE TABLE IF NOT EXISTS users (ID INT PRIMARY KEY AUTO_INCREMENT , NAME VARCHAR(20), LASTNAME VARCHAR(20), AGE INT)";
+
+        statement.execute(comand);
+        statement.close();
 
     }
 
-    public void createUsersTable() {
-
+    public void dropUsersTable() throws SQLException {
+        comand = "DROP TABLE users";
+        statement.execute(comand);
+        statement.close();
     }
 
-    public void dropUsersTable() {
+    public void saveUser(String name, String lastName, byte age) throws SQLException, ClassNotFoundException {
+        PreparedStatement preparedStatement;
+        comand = "INSERT INTO users (name, lastname, age) VALUES( ?, ?, ?)";
+        preparedStatement = Util.getConnection().prepareStatement(comand);
+        preparedStatement.setLong(1, 1);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setByte(3, age);
 
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void removeUserById(long id) throws SQLException, ClassNotFoundException {
+        PreparedStatement preparedStatement;
+        comand = "DELETE FROM users WHERE ID=?";
+        preparedStatement = Util.getConnection().prepareStatement(comand);
+        preparedStatement.setLong(1, id);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
     }
 
-    public void removeUserById(long id) {
-
+    public List<User> getAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        comand = "SELECT id, name, lastname, age";
+        ResultSet rs = statement.executeQuery(comand);
+        while (rs.next()) {
+           users.add((User) rs);
+        }
+        statement.close();
+        return users;
     }
 
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    public void cleanUsersTable() {
-
+    public void cleanUsersTable() throws SQLException {
+        comand = "TRUNCATE TABLE users";
+        statement.execute(comand);
+        statement.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String PASSWORD = "root";
+    private static final String USERNAME = "root";
+
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+         Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+         System.out.println(" Connection OK");
+         return connection;
+    }
 }
